1676b30b075bfc61c7af414a2d568b2f02e87066
SANATH ZINGADE <SANATH ZINGADE@192.168.0.153>
2022-12-29 22:56:30 +0530

    FIRST COMMIT

---
+++ src/main/java/com/maruti/person/models/Person.java
@@ -0,0 +1,56 @@
+package com.maruti.person.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Person {
+	
+	@Id
+	@GeneratedValue
+	
+	private Integer id;
+	
+	@Column
+	private String name;
+	
+	@Column
+	private Integer salary;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getSalary() {
+		return salary;
+	}
+
+	public void setSalary(Integer salary) {
+		this.salary = salary;
+	}
+	
+	
+	
+
+}
